5ff7f901575130b9b5ebe631a2871529b34a7525
Troels Lund <trolund@gmail.com>
2020-10-11 22:53:25 +0200

    coliders on pips added

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,8 @@ import 'phaser';
 
 const height = 600;
 const width = 800;
+const spaceBetweenPipsX = 260;
+const spaceBetweenPipsY = 350;
 
 type Pipe = {
     offset: number;
@@ -11,9 +13,13 @@ type Pipe = {
 
 export default class Demo extends Phaser.Scene {
     bird: Phaser.Physics.Arcade.Sprite = null;
+    pointLabel: Phaser.GameObjects.Text = null;
     xScroll: number = 0;
     lastPipePoint: number = 0;
-    aktivePipes: Pipe[] = []
+    aktivePipes: Pipe[] = [];
+    gameOver: boolean = false;
+    group: any;
+    pipsColiders: Phaser.Physics.Arcade.Group;
 
     constructor() {
         super('demo');
@@ -25,42 +31,64 @@ export default class Demo extends Phaser.Scene {
     }
 
     create() {
-        this.bird = this.physics.add.sprite(100, 400, "bird");
-        this.bird.setScale(0.1);
-        this.bird.setGravity(0, 200);
+        this.pointLabel = this.add.text(30, 30, '', { font: '16px Courier', fill: '#00ff00' });
+
+        this.bird = this.physics.add.sprite(100, 400, "bird").setScale(0.1).setGravity(0, 200);
 
         this.input.keyboard.on('keydown-' + 'SPACE', (event) => {
-            console.log(event);
+            if(!this.gameOver){
             this.bird.setVelocityY(-240);
+            }
         });
     }
 
     update() {
+        if(!this.gameOver){
         this.xScroll--;
         this.bird.setRotation(this.bird.body.velocity.y * 0.02 + 10)
 
         if(this.lastPipePoint > this.xScroll){
             this.createPips();
-            this.lastPipePoint = this.xScroll - 200;
+            this.lastPipePoint = this.xScroll - spaceBetweenPipsX;
         }
 
         this.aktivePipes.forEach(p => {
             p.bottomPipe.setX(this.xScroll + p.offset)
             p.topPipe.setX(this.xScroll + p.offset)
         });
+
+        this.pointLabel.setText(
+            'Points: ' + Math.abs(this.xScroll)
+        );
+
+        this.gameoverCheck();
+        }
+    }
+
+    gameoverCheck() {
+        if(this.bird.y > height || this.bird.y < 0){
+            this.setGameOver();
+        }
+    }
+
+    setGameOver() {
+        this.add.text(width / 2, height / 2, 'Game Over', { font: '16px Courier', fill: '#00ff00' });
+        this.game.loop.pause();
+        this.gameOver = true;
     }
 
     createPips() {
         var xRandom = Phaser.Math.Between(150, height - 150);
 
-        let bottomPipe = this.physics.add.sprite(width, xRandom + 400, "pipe");
-        bottomPipe.setScale(0.26);
+        let bottomPipe = this.physics.add.sprite(width + 50, xRandom + spaceBetweenPipsY, "pipe")
+                        .setScale(0.26);
 
-        let topPipe = this.physics.add.sprite(width, xRandom - 300, "pipe");
-        topPipe.setScale(0.26);
-        topPipe.setRotation(Math.PI);
+        let topPipe = this.physics.add.sprite(width + 50, xRandom - spaceBetweenPipsY, "pipe")
+                        .setScale(0.26)
+                        .setRotation(Math.PI);
 
-        this.aktivePipes.push({bottomPipe, topPipe, offset: Math.abs(this.xScroll) + width} as Pipe);
+        this.physics.add.collider(this.bird, [topPipe, bottomPipe], () => this.setGameOver());
+        this.aktivePipes.push({bottomPipe, topPipe, offset: Math.abs(this.xScroll) + width + 50} as Pipe);
     }
 
 }
